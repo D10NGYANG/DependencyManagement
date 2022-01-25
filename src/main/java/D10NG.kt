74bb1b1e5e09874c94319cd6d26a045a1cbc17cb
D10NG <D10NGYANG@163.com>
2022-01-25 09:26:23 +0800

    更新D10NG的dateUtil和basicJetpackComposeApp；

--- src/main/java/D10NG.kt
+++ src/main/java/D10NG.kt
@@ -1,7 +1,7 @@
 object D10NG {
     const val appUtil = "com.github.D10NGYANG:DLAppUtil:1.7"
     const val coroutinesUtil = "com.github.D10NGYANG:DLCoroutinesUtil:0.2"
-    const val dateUtil = "com.github.D10NGYANG:DLDateUtil:1.3"
+    const val dateUtil = "com.github.D10NGYANG:DLDateUtil:1.4"
     const val date_fix_8_coreLibraryDesugaring = "com.android.tools:desugar_jdk_libs:1.1.5"
     const val stringUtil = "com.github.D10NGYANG:DLStringUtil:1.12"
     const val textUtil = "com.github.D10NGYANG:DLTextUtil:1.2.3"
@@ -10,5 +10,5 @@ object D10NG {
     const val retrofitCoroutines = "com.github.D10NGYANG:DL10RetrofitCoroutines:1.4"
     const val moshiUtil = "com.github.D10NGYANG:DLMoshiUtil:1.2"
     const val sideBar_compose = "com.github.D10NGYANG:DLSideBar_Compose:1.0.2"
-    const val basicJetpackComposeApp = "com.github.D10NGYANG:DLBasicJetpackComposeApp:1.0.2"
+    const val basicJetpackComposeApp = "com.github.D10NGYANG:DLBasicJetpackComposeApp:1.0.3"
 }
